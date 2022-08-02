8ac162fd8f31f17950089ddf37298b4ac79543a8
lokesh-b1 <lokesh.bellana@imaginnovate.com>
2022-08-02 16:08:25 +0530

    updated mobile no.

--- src/main/java/com/springboot/crud/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/crud/service/impl/EmployeeServiceImpl.java
@@ -41,7 +41,7 @@ public class EmployeeServiceImpl implements EmployeeService{
 		existingEmployee.setFirstName(employee.getFirstName());
 		existingEmployee.setLastName(employee.getLastName());
 		existingEmployee.setEmail(employee.getEmail());
-		//existingEmployee.setPhoneNumber(employee.getPhoneNumber());
+		existingEmployee.setPhoneNumber(employee.getPhoneNumber());
 		
 		// save existing employee to DB
 		employeeRepository.save(existingEmployee);
