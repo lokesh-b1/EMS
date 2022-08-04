a1817ba2b269bb468167143f5df0ca8148a93a70
lokesh-b1 <lokesh.bellana@imaginnovate.com>
2022-08-04 12:49:19 +0530

    changes

--- src/main/java/com/springboot/crud/entity/Employee.java
+++ src/main/java/com/springboot/crud/entity/Employee.java
@@ -1,7 +1,5 @@
 package com.springboot.crud.entity;
 
-import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,88 +10,53 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "employees")
 public class Employee {
-	
-		@Id
-		@GeneratedValue(strategy = GenerationType.AUTO)
-		private long id;
-		
-		@Column
-		private String firstName;
-		
-		@Column
-		private String lastName;
-		
-		@Column
-		private String email;
-		
-		@Column
-		private long phoneNumber;
-		
-		public long getId() {
-			return id;
-		}
 
-		public void setId(long id) {
-			this.id = id;
-		}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
-		public String getFirstName() {
-			return firstName;
-		}
+	@Column
+	private String firstName;
 
-		public void setFirstName(String firstName) {
-			this.firstName = firstName;
-		}
+	@Column
+	private String lastName;
 
-		public String getLastName() {
-			return lastName;
-		}
+	@Column
+	private String email;
 
-		public void setLastName(String lastName) {
-			this.lastName = lastName;
-		}
+	@Column
+	private long phoneNumber;
 
-		public String getEmail() {
-			return email;
-		}
+	public long getId() {
+		return id;
+	}
 
-		public void setEmail(String email) {
-			this.email = email;
-		}
+	public void setId(long id) {
+		this.id = id;
+	}
 
-		public long getPhoneNumber() {
-			return phoneNumber;
-		}
+	public String getFirstName() {
+		return firstName;
+	}
 
-		public void setPhoneNumber(long phoneNumber) {
-			this.phoneNumber = phoneNumber;
-		}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-		}
+	public String getLastName() {
+		return lastName;
+	}
 
-<<<<<<< HEAD
-=======
-		@Override
-		public int hashCode() {
-			return Objects.hash(email, firstName, id, lastName, phoneNumber);
-		}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Employee other = (Employee) obj;
-			return Objects.equals(email, other.email) && Objects.equals(firstName, other.firstName) && id == other.id
-					&& Objects.equals(lastName, other.lastName) && phoneNumber == other.phoneNumber;
-		}
-		
+	public String getEmail() {
+		return email;
+	}
 
-		 
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
-
->>>>>>> Pandu
